3bd449bd1e5f2feb018fd6102ae67963a24f2a69
Robert Cole <robertcole@Roberts-MBP.fios-router.home>
2018-09-23 21:13:52 -0400

    Add number branch and variables

---
+++ main.js
@@ -0,0 +1,10 @@
+var num = 5;
+console.log(num);
+
+var numAddition = 6 + 4;
+console.log(numAddition);
+
+var numConcatOne = 3;
+var numConcatTwo = "9";
+
+console.log(numConcatOne + numConcatTwo);
